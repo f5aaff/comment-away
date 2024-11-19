54b34e7c4de7a61be8dc16d72f23fe08b3f8467a
f5aaff <f5aaff@git>
2024-11-19 08:42:58 +0000

    turned scripty main into functions, needs I/O

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,51 @@
 use libloading::{Library, Symbol};
+use std::ffi::CString;
 use tree_sitter::{Language, Parser};
 
-
-// load the so using the library::new() from libloading.
+// loads the library from the given shared object, wrapped to produce a Result.
 fn load_lib_so(path: String) -> Result<Library, anyhow::Error> {
     let library = unsafe { Library::new(path)? };
 
     Ok(library)
 }
 
-// load the language from the library taken through libloading,
-// uses an unsafe extern call to C, getting the library by name.
-fn load_language(library: Library, name: &[u8]) -> Result<Language, anyhow::Error> {
+// load the language from the library, converting the name str to a CString
+// for the null terminating byte.
+fn load_language(library: &Library, name: &str) -> Result<Language, anyhow::Error> {
+    // Append a null terminator to the name and convert it to CString
+    let c_name = CString::new(name)?;
+
+    // Get the byte slice with a null terminator
+    let bytes = c_name.as_bytes_with_nul();
+
     let language: Language = unsafe {
-        let func: Symbol<unsafe extern "C" fn() -> Language> = library.get(name)?;
+        let func: Symbol<unsafe extern "C" fn() -> Language> = library
+            .get(bytes)
+            .expect("Failed to load language function");
         func()
     };
 
     Ok(language)
 }
 
-// create a parser for the given language
+// creates a parser from the given language, wrapped to produce a Result.
 fn create_parser(language: Language) -> Result<Parser, anyhow::Error> {
     let mut parser = Parser::new();
-    match parser.set_language(&language){
-        Ok(()) => {
-            return Ok(parser)
-        },
-        Err(e) => {
-            return Err(e.into())
-        }
-    }
+    parser.set_language(&language)?;
+
+    Ok(parser)
 }
 
-// generate a tree using the parser, produces a tree_sitter::Tree, from which
-// the root_node can be obtained.
-fn gen_tree(mut parser: Parser, source_code: &str) -> tree_sitter::Tree {
+fn gen_tree(mut parser:Parser,source_code: &str) -> tree_sitter::Tree {
+
     let tree = parser
         .parse(source_code, None)
         .expect("Failed to parse source code");
     tree
 }
 
-fn traverse_tree(node: tree_sitter::Node, source_code: &str,node_kind:String) {
-    if node.kind() == &node_kind {
+fn traverse_tree(node: tree_sitter::Node, source_code: &str) {
+    if node.kind() == "comment" {
         println!(
             "Found comment: {}",
             &source_code[node.start_byte()..node.end_byte()]
@@ -51,35 +53,23 @@ fn traverse_tree(node: tree_sitter::Node, source_code: &str,node_kind:String) {
     }
 
     for child in node.children(&mut node.walk()) {
-        traverse_tree(child, source_code,node_kind.clone());
+        traverse_tree(child, source_code);
     }
 }
 
 fn main() {
     // Path to the shared library
-    println!("attempting to load library...");
     let library_path = "/home/f/dev/cas/comment-away/src/shared_libs/libjavascript.so";
-    let library = match load_lib_so(library_path.to_string()) {
-        Ok(l) => l,
-        Err(e) => {
-            panic!("error loading library from so: {}", e)
-        }
-    };
-    println!("attempting to instantiate language...");
-    // Correctly load the language function
-    let language = match load_language(library, b"tree_sitter_javascript") {
-        Ok(l) => l,
-        Err(e) => {
-            panic!("error loading language from library: {}", e)
-        }
-    };
-    println!("attempting to instantiate parser...");
-    let parser = match create_parser(language) {
-        Ok(p) => p,
-        Err(e) => {
-            panic!("error generating parser: {}", e)
-        }
-    };
+
+    // load the library from the shared object
+    let library = load_lib_so(library_path.to_string()).expect("Failed to load library");
+
+    // load the language from the library
+    let language =
+        load_language(&library, "tree_sitter_javascript").expect("Failed to load language");
+
+    // instantiate a parser from the given language
+    let parser = create_parser(language).expect("Failed to set language");
 
     let source_code = r#"
     // This is a comment
@@ -88,11 +78,13 @@ fn main() {
     }
     "#;
 
-    println!("attempting to generate tree...");
+    // generate a tree_sitter::Tree from the source, using the parser
     let tree = gen_tree(parser, source_code);
+
+    // grab the root node from the tree
     let root_node = tree.root_node();
 
-    println!("attempting to traverse tree...");
     // Traverse and find comments
-    traverse_tree(root_node, source_code,String::from("comment"));
+    traverse_tree(root_node, source_code);
 }
+
